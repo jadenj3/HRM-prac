53fe067748f924f84cceba06e70d5fa0065265c4
Jaden Johnson <jadenjohnson@fb.com>
2025-07-26 21:26:39 -0400

    first pass, add value embedding, set it to 0

--- pretrain.py
+++ pretrain.py
@@ -141,12 +141,6 @@ def create_model(config: PretrainConfig, train_metadata: PuzzleDatasetMetadata, 
             weight_decay=config.puzzle_emb_weight_decay,
             world_size=world_size
         ),
-        CastedSparseEmbeddingSignSGD_Distributed(
-            model.model.inner.value_puzzle_emb.buffers(),
-            lr=0,
-            weight_decay=config.puzzle_emb_weight_decay,
-            world_size=world_size
-        ),
         AdamATan2(
             model.parameters(),
 
