f3f45866063827f35b79ebcbd5f7bcec1faeea68
Jaden Johnson <jadenjohnson@fb.com>
2025-07-27 20:06:10 -0400

    first pass, add value embedding, re-add it

--- models/layers.py
+++ models/layers.py
@@ -105,7 +105,7 @@ class Attention(nn.Module):
         self.num_heads = num_heads
         self.num_key_value_heads = num_key_value_heads
         self.causal = causal
-        self.lambdas = nn.Parameter(torch.tensor([0.5, 0.5]))
+        self.lambdas = nn.Parameter(torch.tensor([0.9, 0.1]))
         self.qkv_proj = CastedLinear(self.hidden_size, (self.num_heads + 2 * self.num_key_value_heads) * self.head_dim, bias=False)
         self.o_proj = CastedLinear(self.output_size, self.hidden_size, bias=False)
 
