a4240457a5ec5f1771f022d6e8e73f9b2d4e1e5f
Jaden Johnson <jadenjohnson@fb.com>
2025-07-30 09:04:18 -0400

    value emb, 0.5. MUON! 0.0025 LR

--- pretrain.py
+++ pretrain.py
@@ -279,7 +279,7 @@ def create_model(config: PretrainConfig, train_metadata: PuzzleDatasetMetadata, 
     
     optimizer_lrs = [
         config.puzzle_emb_lr,
-        0.025,
+        0.0025,
         config.lr
     ][:len(optimizers)]
     
