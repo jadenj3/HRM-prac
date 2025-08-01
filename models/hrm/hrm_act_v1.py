851ad4ee40629cf7fba72658adac99ca6185a7df
Jaden Johnson <jadenjohnson@fb.com>
2025-08-01 19:15:06 -0400

    value emb, 0.5. Warmup then linear decay LR. Readd Q-learning

--- models/hrm/hrm_act_v1.py
+++ models/hrm/hrm_act_v1.py
@@ -268,6 +268,22 @@ class HierarchicalReasoningModel_ACTV1(nn.Module):
             
             halted = is_last_step
 
-            # Q-learning disabled - always run for max steps
+            if self.training and (self.config.halt_max_steps > 1):
+                # Halt signal
+                # NOTE: During evaluation, always use max steps, this is to guarantee the same halting steps inside a batch for batching purposes
+                halted = halted | (q_halt_logits > q_continue_logits)
+
+                # Exploration
+                min_halt_steps = (torch.rand_like(q_halt_logits) < self.config.halt_exploration_prob) * torch.randint_like(new_steps, low=2, high=self.config.halt_max_steps + 1)
+
+                halted = halted & (new_steps >= min_halt_steps)
+
+                # Compute target Q
+                # NOTE: No replay buffer and target networks for computing target Q-value.
+                # As batch_size is large, there're many parallel envs.
+                # Similar concept as PQN https://arxiv.org/abs/2407.04811
+                next_q_halt_logits, next_q_continue_logits = self.inner(new_inner_carry, new_current_data)[-1]
+
+                outputs["target_q_continue"] = torch.sigmoid(torch.where(is_last_step, next_q_halt_logits, torch.maximum(next_q_halt_logits, next_q_continue_logits)))
 
         return HierarchicalReasoningModel_ACTV1Carry(new_inner_carry, new_steps, halted, new_current_data), outputs
--- models/losses.py
+++ models/losses.py
@@ -93,22 +93,24 @@ class ACTLossHead(nn.Module):
         # Losses
         # FIXME: Assuming the batch is always full
         lm_loss = (self.loss_fn(outputs["logits"], labels, ignore_index=IGNORE_LABEL_ID) / loss_divisor).sum()
-        
-        # Q-learning disabled - skip Q-loss computations
-        # q_halt_loss = F.binary_cross_entropy_with_logits(outputs["q_halt_logits"], seq_is_correct.to(outputs["q_halt_logits"].dtype), reduction="sum")
+        q_halt_loss = F.binary_cross_entropy_with_logits(outputs["q_halt_logits"],
+                                                         seq_is_correct.to(outputs["q_halt_logits"].dtype),
+                                                         reduction="sum")
 
         metrics.update({
             "lm_loss": lm_loss.detach(),
-            # "q_halt_loss": q_halt_loss.detach(),
+            "q_halt_loss": q_halt_loss.detach(),
         })
 
-        # Q continue (bootstrapping target loss) - disabled
-        # q_continue_loss = 0
-        # if "target_q_continue" in outputs:
-        #     q_continue_loss = F.binary_cross_entropy_with_logits(outputs["q_continue_logits"], outputs["target_q_continue"], reduction="sum")
-        #     metrics["q_continue_loss"] = q_continue_loss.detach()
+        # Q continue (bootstrapping target loss)
+        q_continue_loss = 0
+        if "target_q_continue" in outputs:
+            q_continue_loss = F.binary_cross_entropy_with_logits(outputs["q_continue_logits"],
+                                                                 outputs["target_q_continue"], reduction="sum")
+
+            metrics["q_continue_loss"] = q_continue_loss.detach()
 
         # Filter outputs for return
         detached_outputs = {k: outputs[k].detach() for k in return_keys if k in outputs}
 
-        return new_carry, lm_loss, metrics, detached_outputs, new_carry.halted.all()
+        return new_carry, lm_loss + 0.5 * (q_halt_loss + q_continue_loss), metrics, detached_outputs, new_carry.halted.all()
